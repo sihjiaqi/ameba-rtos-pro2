95735b0d3d5dd50cd7fec7bf4042c04f2ff98c80
Sih Jia Qi <sihjiaqi@realtek-sg.com>
2025-06-13 15:48:37 +0800

    Fix code

--- .github/scripts/python/bootstrap-windows-env.py
+++ .github/scripts/python/bootstrap-windows-env.py
@@ -73,14 +73,6 @@ def add_env_to_bashrc():
         with open(BASHRC_PATH, "a") as f:
             f.write("\n".join(new_lines) + "\n")
 
-def install_build_tools():
-    print("Installing build tools...")
-    subprocess.run(
-        ["winget", "install", "--id", "Arm.GnuArmEmbeddedToolchain", "-e", 
-         "--accept-package-agreements", "--accept-source-agreements"],
-        check=True
-    )
-
 def main():
     try:
         download_extract_msys()
@@ -88,7 +80,6 @@ def main():
         launch_msys_shell() 
         install_cmake()
         add_env_to_bashrc() 
-        install_build_tools()
     except Exception as e:
         print(f"An error occurred: {e}")
         exit(1)
