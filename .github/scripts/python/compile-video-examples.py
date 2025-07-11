0b9b09bbbc144619e41281e753ee469a435f9c6c
Sih Jia Qi <sihjiaqi@realtek-sg.com>
2025-07-11 13:49:28 +0800

    Fix: Create output directory before storing

--- .github/scripts/python/compile-video-examples.py
+++ .github/scripts/python/compile-video-examples.py
@@ -103,9 +103,9 @@ def build_example(example):
     # Copy built binary file to output directory
     built_bin_name = "flash_ntz.nn.bin" if "nn" in example.lower() else "flash_ntz.bin"
     built_bin_path = os.path.join(build_dir, built_bin_name)
-    output_bin_path = os.path.join(BIN_OUTPUT_DIR, f"{example}", built_bin_name)
-    # output_bin_path = os.path.join(BIN_OUTPUT_DIR, f"{example}.bin")
-    os.makedirs(BIN_OUTPUT_DIR, exist_ok=True)
+    output_example_dir = os.path.join(BIN_OUTPUT_DIR, f"{example}")
+    output_bin_path = os.path.join(output_example_dir, built_bin_name)
+    os.makedirs(output_example_dir, exist_ok=True)
     shutil.copyfile(built_bin_path, output_bin_path)
     print(f"Saving built binary to: {output_bin_path}")
     
