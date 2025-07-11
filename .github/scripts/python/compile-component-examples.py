d80a844ada6116a38b32609e65c9ff58659af3eb
Sih Jia Qi <sihjiaqi@realtek-sg.com>
2025-07-11 14:02:31 +0800

    Chore: Update path for storing output firmware

--- .github/scripts/python/compile-component-examples.py
+++ .github/scripts/python/compile-component-examples.py
@@ -31,9 +31,11 @@ def build_example(example):
     # Copy built binary file to output directory
     built_bin_name = "flash_ntz.bin"
     built_bin_path = os.path.join(build_dir, built_bin_name)
-    output_bin_path = os.path.join(BIN_OUTPUT_DIR, f"{example}.bin")
-    os.makedirs(BIN_OUTPUT_DIR, exist_ok=True)
+    output_example_dir = os.path.join(BIN_OUTPUT_DIR, f"{example}")
+    output_bin_path = os.path.join(output_example_dir, built_bin_name)
+    os.makedirs(output_example_dir, exist_ok=True)
     shutil.copyfile(built_bin_path, output_bin_path)
+    print(f"Saving built binary to: {output_bin_path}")
     
     # Clean for next build
     run('make clean', cwd=build_dir)
